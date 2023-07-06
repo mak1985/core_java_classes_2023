5dae11281b9847cb399810384baaaa32e519417d
Makhan AutomationHub <makhan.dhar85@gmail.com>
2023-07-06 11:54:22 +0530

    Initial commit

--- src/com/company_name/day01/MyFirstJavaCode.java
+++ src/com/company_name/day01/MyFirstJavaCode.java
@@ -1,2 +1,8 @@
-package com.company_name.day01;public class MyFirstJavaCode {
+package com.company_name.day01;
+
+public class MyFirstJavaCode {
+    public static void main(String[] args) {
+        //System.out.println("I am learning coding"); //cursor will be placed to next line
+        System.out.print("I am learning coding");
+    }
 }
--- src/com/company_name/day02/DataTypes.java
+++ src/com/company_name/day02/DataTypes.java
@@ -1,2 +1,27 @@
-package com.company_name.day02;public class DataTypes {
+package com.company_name.day02;
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+    // 1. primitive data types - emp data
+
+        int mobile_number = 1234578951;  //size 4 bytes
+        long emp_data = 1234567895100000000l;
+        double salary = 7000000.10; //size 8 bytes
+        char g = 'M';
+        boolean b = true;
+        float f = 7000000.10f;
+
+        byte b1 = 100;
+        short s = 4000;
+
+        System.out.println(mobile_number);
+        System.out.println(salary);
+        System.out.println(g);
+        System.out.println(b);
+        System.out.println(f);
+        System.out.println(emp_data);
+        System.out.println(b1);
+        System.out.println(s);
+    }
 }
