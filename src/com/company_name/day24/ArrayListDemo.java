072dae7628764badf16b12f3c1bb7eb02f8cae3a
Makhan AutomationHub <makhan.dhar85@gmail.com>
2023-08-14 10:04:10 +0530

    Changes

--- src/com/company_name/day24/ArrayListDemo.java
+++ src/com/company_name/day24/ArrayListDemo.java
@@ -4,14 +4,10 @@ import java.util.ArrayList;
 
 public class ArrayListDemo {
     public static void main(String[] args) {
-
         //Declare ArrayList - approach 1
-
         ArrayList obj = new ArrayList(); //All types of data. (Homogeneous+heterogeneous)
-
         //Declare ArrayList - approach 2
         ArrayList <String> obj1 = new ArrayList<String>(); //Homogeneous
-
         //Adding data to ArrayList
         /**
          * 1. add(object)
@@ -26,8 +22,8 @@ public class ArrayListDemo {
          * 10. addAll() - Add one collection to another collection
          * 11. removeAll() - To remove inserted collection
          */
-        obj.add("Java");
-        obj.add(100);
+        obj.add("Java"); //0th position
+        obj.add(100); //1st position
         obj.add('A');
         obj.add(true);
         obj.add(12.4);
@@ -35,10 +31,34 @@ public class ArrayListDemo {
         obj.add(12.4);
         //print ArrayList
         System.out.println(obj);
-
         //print size
         System.out.println(obj.size());
-
         System.out.println(obj1.size());
+        //remove
+        obj.remove(6);
+        System.out.println(obj);
+
+        //get method
+        System.out.println(obj.get(1));
+        //set method
+        obj.set(5, "C++");
+        System.out.println(obj);
+
+        System.out.println(obj.size());
+
+        //Insert element in arrayList specific position
+        obj.add(1, 1000);
+        System.out.println(obj);
+
+        //contains() -
+        System.out.println(obj.contains("C++"));
+
+        //isEmpty()
+        System.out.println(obj.isEmpty());
+
+        //clear()
+        obj.clear();
+        System.out.println(obj);
+        System.out.println(obj.isEmpty());
     }
 }
---
+++ src/com/company_name/day25/ArrayListDemo2.java
@@ -0,0 +1,64 @@
+package com.company_name.day25;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class ArrayListDemo2 {
+    public static void main(String[] args) {
+        //Declare arraylist(collection)
+        //ArrayList <String> obj = new ArrayList<>();
+        ArrayList obj = new ArrayList<>();
+        obj.add("B");
+        obj.add("A");
+        obj.add("C");
+        //obj.add(100);
+
+
+        System.out.println(obj);
+
+        //AddAll ()
+        ArrayList <String> obj1 = new ArrayList<>();
+        obj1.addAll(obj);
+        System.out.println("After adding collection as obj to obj1 " + obj1); //[S, A, M]
+
+        //removeAll()
+        obj1.removeAll(obj);
+        System.out.println("After removing collection "+ obj1);
+
+        //How to use loop through elements of array list
+        /**
+         * for loop
+         * for each (enhanched for loop
+         * iterator
+         */
+
+//        for (int i = 0; i <obj.size(); i++) {
+//            System.out.println(obj.get(i));
+//        }
+
+//        for (Object s : obj) {
+//            System.out.println(s);
+//        }
+
+//        Iterator it = obj.iterator();
+//        while (it.hasNext()) {
+//            System.out.println(it.next());
+//        }
+//
+
+        //How to sort arrayList using Collections class
+        System.out.println("Before sorting " + obj); //B, A, C
+
+        Collections.sort(obj);
+        System.out.println("After sorting " + obj); //A, B, C
+
+        //sorting arrayList in reverse order
+        Collections.sort(obj, Collections.reverseOrder());
+        System.out.println("After reverse order sorting " + obj); //C, B, A
+
+        //shuffle()
+        Collections.shuffle(obj);
+        System.out.println("After shuffling " +obj); //A, C, B,
+    }
+}
